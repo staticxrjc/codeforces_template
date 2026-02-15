eef37d11c62177b41b7dd49349090fe89af79f5e
staticxrjc <staticxrjc@gmail.com>
2026-02-14 18:47:07 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <cassert>
+#include <iostream>
+#define LOCAL
+
+long long solve(int n, std::vector<int> a) {
+}
+
+#ifdef LOCAL
+#define assertm(cond, msg) assert((cond) && msg)
+#else
+#define assertm(cond, msg)
+#endif
+
+void self_test() {
+    assertm(solve(3, {1,2,3}) == 6,  "basic sum");
+    assertm(solve(1, {5})     == 5,  "single element");
+
+    std::cout << "All self-tests passed ✓\n";
+}
+
+int main() {
+#ifdef LOCAL
+    self_test();
+    return EXIT_SUCCESS;
+#endif
+}
